c24a509ce3c9b6aeadf73eef5e3b553db56a3350
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-06-23 14:39:37 +0300

    First and second tasks

---
+++ bootcamp/materials/1-dimensional-data-modeling/homework/homework-lab/actors_ddl.sql
@@ -0,0 +1,21 @@
+CREATE TYPE films AS (
+    film text,
+    votes INTEGER,
+    rating REAL,
+    filmid text
+);
+CREATE TYPE quality_class AS ENUM (
+    'star','good','average','bad'
+);
+
+CREATE TABLE actors (
+    actorid text ,
+    actor text,
+    films films[],
+    quality_class quality_class,
+    is_active BOOLEAN,
+    year INTEGER,
+    PRIMARY KEY (actorid,year)
+);
+
+SELECT * FROM actor_films WHERE year = 2021;
---
+++ bootcamp/materials/1-dimensional-data-modeling/homework/homework-lab/cumulative_table_gen_query.sql
@@ -0,0 +1,21 @@
+INSERT INTO actors
+WITH actors_year AS(
+    SELECT actorid,actor,MAX(year) as year FROM actor_films GROUP BY actorid,actor
+)
+select actorid,MAX(actor) as actor,
+       ARRAY_AGG(
+               ROW (
+                   film,
+                   votes,
+                   rating,
+                   filmid
+                   )::films
+       ),
+        CASE WHEN  AVG(rating) > 8 THEN 'star'::quality_class
+            WHEN avg(rating) > 7 AND avg(rating) <= 8 THEN 'good'::quality_class
+            WHEN avg(rating) > 6 and avg(rating) <= 7 THEN 'average'::quality_class
+            WHEN avg(rating) <= 6 THEN 'bad'::quality_class END as quality_class,
+        CASE WHEN (SELECT year FROM actors_year WHERE actor_films.actorid = actors_year.actorid) = 2021 THEN true ELSE false END as is_active,
+        year
+from actor_films WHERE year = 1985
+group by actorid,year;
