fbaa724796ec5bfab17378241f92c53004330531
RANDRIANARISOA Ny Avohasina Mampandry <120253454+N1A1voIt@users.noreply.github.com>
2025-06-23 14:44:25 +0300

    Actors history dll

---
+++ bootcamp/materials/1-dimensional-data-modeling/homework/homework-lab/actors_history_scd_dll.sql
@@ -0,0 +1,8 @@
+CREATE TABLE actors_history_scd (
+    actorid text,
+    actorname text,
+    start_date date,
+    end_date date DEFAULT '9999-10-21',
+    quality_class quality_class,
+    is_active boolean
+);
